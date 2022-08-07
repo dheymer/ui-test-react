1c9e7094bc48e808f39a7395974e0070a6888e45
Dheymer Leon <dheymer@gmail.com>
2022-08-07 11:28:14 -0500

    Creating the components for the structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import React from 'react';
+import PreviousPolls from './components/PreviousPolls';
 
-function App() {
-  return (
-    <div className="App">
-      App Component
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <div className='app_title'>
+          <h2>Previous Rulings</h2>
+          <select id='display-mode' onChange={this.onSelectChange}>
+            <option value='grid'>Grid</option>
+            <option value='list'>List</option>
+          </select>
+        </div>
+        <PreviousPolls />
+      </div>
+    );
+  }
 }
 
 export default App;
