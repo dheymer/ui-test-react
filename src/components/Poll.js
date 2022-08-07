b35793b592baeddcd1c6ad9beac81adf5dc71264
Dheymer Leon <dheymer@gmail.com>
2022-08-07 14:48:00 -0500

    Added styles to grid layout for tablet and desktop

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -4,6 +4,7 @@ const Poll = ({position, data}) => {
     const totalVotes = data.votes.negative + data.votes.positive;
     const likeRate = ((data.votes.positive / totalVotes) * 100).toFixed(1);     // Percentage of positive votes
     const dislikeRate = ((data.votes.negative / totalVotes) * 100).toFixed(1);  // Percentage of negative votes
+    let vote = '';                                                              // Value of the user's vote
 
     /**
      * Obtains the general opinion based on votes
@@ -31,6 +32,16 @@ const Poll = ({position, data}) => {
         return relativeTime.format(diffDays, 'day');
     };
 
+    const setVote = (selectedVote) => {
+        vote = selectedVote;
+        document.getElementById(`vote-${position}-${selectedVote}`).classList.add('selected');
+        if(selectedVote === 'like') {
+            document.getElementById(`vote-${position}-dislike`).classList.remove('selected');
+        } else {
+            document.getElementById(`vote-${position}-like`).classList.remove('selected');
+        }
+    }
+
     return (
         <div className="poll" style={{ backgroundImage: `url('../assets/img/${data.picture}')` }}>
             <div className="gradient-bg">
@@ -45,10 +56,10 @@ const Poll = ({position, data}) => {
                         </div>
                         <p className="bottom-note">{`${relativeDays(new Date(data.lastUpdated))} in `} <span style={{textTransform:'capitalize'}}>{data.category}</span></p>
                         <div className="vote-form">
-                            <span className="like">
+                            <span id={`vote-${position}-like`} className="like" onClick={() => setVote('like')}>
                                 <img src="../assets/img/thumbs-up.svg" />
                             </span>
-                            <span className="dislike">
+                            <span id={`vote-${position}-dislike`} className="dislike" onClick={() => setVote('dislike')}>
                                 <img src="../assets/img/thumbs-down.svg" />
                             </span>
                             <button>Vote Now</button>
